2617d9f7781aeff4e8ed775384f04fb32728438c
jyhwng <jy.hwng@gmail.com>
2019-09-22 01:38:55 +0900

    Create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import styled from "styled-components";
+
+export const Header: React.FC = () => (
+  <Base>
+    <h1>🚀 SpaceX Monitor</h1>
+  </Base>
+);
+
+const Base = styled.header`
+  top: 0;
+  margin: 0 auto;
+  max-width: 900px;
+  position: sticky;
+  padding: 36px 0 24px;
+  background-color: #282c34;
+  h1 {
+    margin: 0;
+    color: #fff;
+    font-size: 1.3em;
+  }
+`;
