92b2d9b3a93e9f3f5c8f02066c364275491ed794
jyhwng <jy.hwng@gmail.com>
2019-09-22 23:09:24 +0900

    Create Launches list component

---
+++ src/components/Launches/index.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { LaunchInfo } from "../../types";
+import styled from "styled-components";
+import { Launch } from "../Launch";
+import { getLaunches } from "../../services/actions/launchActions";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../services/types";
+
+interface Props {}
+
+export const Launches: React.FC<Props> = () => {
+  const dispatch = useDispatch();
+  const { launches } = useSelector((state: RootState) => state.launch);
+
+  React.useEffect(() => {
+    dispatch(getLaunches());
+  }, []);
+
+  return (
+    <Ul>
+      {launches.map((launch: LaunchInfo, index: number) => (
+        <React.Fragment key={index}>
+          {(index + 1) % 2 === 0 && <RowGap />}
+          <Li>
+            <Launch launch={launch} />
+          </Li>
+          {(index + 1) % 2 === 0 && <Linebreak />}
+        </React.Fragment>
+      ))}
+    </Ul>
+  );
+};
+
+const Ul = styled.ul`
+  display: flex;
+  margin-top: 0;
+  padding-left: 0;
+  flex-wrap: wrap;
+  list-style: none;
+`;
+
+const Li = styled.li`
+  flex: 1;
+`;
+
+const RowGap = styled.div`
+  width: 16px;
+`;
+
+const Linebreak = styled.div`
+  width: 0;
+  height: 16px;
+  flex-basis: 100%;
+`;
