2e3015a76c7d5b1131d65ccee548a64e758fd5f1
jyhwng <jy.hwng@gmail.com>
2019-09-22 20:34:46 +0900

    Create api.ts

---
+++ src/services/apis.ts
@@ -0,0 +1,9 @@
+import Axios from "axios";
+const camelcaseKeys = require("camelcase-keys");
+
+Axios.defaults.baseURL = "https://api.spacexdata.com/v3";
+
+export const fetchLaunches = async (query?: string) => {
+  const res = await Axios.get(`/launches?limit=50${query}`);
+  return camelcaseKeys(res.data, { deep: true });
+};
