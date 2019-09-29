e621871a47c503cb8b18adbcbda1e37e0eb9451b
jyhwng <jy.hwng@gmail.com>
2019-09-29 21:31:16 +0900

    Replace useState isChecked to store value

--- src/components/LaunchSuccessFilter/index.tsx
+++ src/components/LaunchSuccessFilter/index.tsx
@@ -1,22 +1,23 @@
 import * as React from "react";
 import { Checkbox } from "../Checkbox";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setQuery } from "../../services/actions/queryActions";
 import { getLaunches } from "../../services/actions/launchActions";
 import { Fieldset } from "../Fieldset";
+import { RootState } from "../../services/types";
 
 export const LaunchSuccessFilter: React.FC = () => {
-  const [isChecked, setIsChecked] = React.useState(true);
   const dispatch = useDispatch();
+  const isChecked = useSelector((state: RootState) => state.query.launchSuccess);
 
-  React.useEffect(() => {
-    dispatch(setQuery({ launchSuccess: isChecked }));
+  const _onChange = ({ currentTarget }: React.FormEvent<HTMLInputElement>) => {
+    dispatch(setQuery({ launchSuccess: currentTarget.checked }));
     dispatch(getLaunches());
-  }, [isChecked]);
+  };
 
   return (
     <Fieldset legend="Filters">
-      <Checkbox checked={isChecked} onChange={e => setIsChecked(e.currentTarget.checked)}>
+      <Checkbox checked={!!isChecked} onChange={e => _onChange(e)}>
         Successful
       </Checkbox>
     </Fieldset>
